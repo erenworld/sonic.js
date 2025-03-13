0df0b07377b8ad351541f1d871704fdf1f0a152a
eren <erenxturkoglu@gmail.com>
2025-03-13 17:41:04 +0100

    package works

--- index.js
+++
@@ -1,8 +0,0 @@
-/*
- * Eren Turkoglu 
- */
-
-const sonic = document.querySelector(".sonic");
-const sec = 5;
-
-sonic.innerHTML = `Page load was ${sec}`
---
+++ src/index.js
@@ -0,0 +1,23 @@
+/*
+ * Eren Turkoglu 
+ */
+
+function performanceTest() {
+    window.addEventListener("load", () => {
+        const sonic = document.querySelector(".sonic");
+        
+        if (!sonic) {
+            console.error("Element not found. Please add .sonic classe to your element.");
+            return false;
+        }
+        if (sonic) {
+            const pageLoad = performance.mark("pageLoad"); 
+            const loadTime = pageLoad.startTime / 1000;
+            const roundedTime = loadTime.toFixed(4);
+            sonic.innerHTML = `Load time is ${roundedTime}`;
+        }
+    })
+}
+performanceTest();
+
+export default performanceTest;
