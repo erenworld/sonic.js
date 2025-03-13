d4cce2c25ec5e75130958af7ad2bc9d94ab6d1d9
eren <erenxturkoglu@gmail.com>
2025-03-13 22:57:57 +0100

    add new package

---
+++ dist/bundle.js
@@ -0,0 +1,56 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((module) => {
+
+eval("function performanceLoad() {\n  window.addEventListener(\"load\", function () {\n    var sonic = document.querySelector(\".sonic\");\n    if (!sonic) {\n      console.error(\"Element not found. Please add .sonic classe to your element.\");\n      return false;\n    }\n    if (sonic) {\n      var pageLoad = performance.mark(\"pageLoad\");\n      var loadTime = pageLoad.startTime / 1000;\n      var roundedTime = loadTime.toFixed(4);\n      sonic.innerHTML = \"Load time is \".concat(roundedTime);\n    }\n  });\n}\nmodule.exports = performanceLoad;\n\n//# sourceURL=webpack://sonicfaster/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+function performanceLoad() {
+    window.addEventListener("load", () => {
+        const sonic = document.querySelector(".sonic");
+        
+        if (!sonic) {
+            console.error("Element not found. Please add .sonic classe to your element.");
+            return false;
+        }
+        if (sonic) {
+            const pageLoad = performance.mark("pageLoad"); 
+            const loadTime = pageLoad.startTime / 1000;
+            const roundedTime = loadTime.toFixed(4);
+            sonic.innerHTML = `Load time is ${roundedTime}`;
+        }
+    })
+}
+
+module.exports = performanceLoad;
